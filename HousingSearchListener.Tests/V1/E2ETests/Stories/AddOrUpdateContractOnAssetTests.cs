df3df71779a6f43d64dabfe1988d40dace47dd82
James Heppinstall <james.heppinstall@hackney.gov.uk>
2025-01-27 10:32:57 +0000

    ts-1923: always update the asset in the index, regardless of contract status

--- HousingSearchListener.Tests/V1/E2ETests/Stories/AddOrUpdateContractOnAssetTests.cs
+++ HousingSearchListener.Tests/V1/E2ETests/Stories/AddOrUpdateContractOnAssetTests.cs
@@ -79,5 +79,21 @@ namespace HousingSearchListener.Tests.V1.E2ETests.Stories
                     _ContractsApiFixture.ResponseObject, _esFixture.ElasticSearchClient))
                 .BDDfy();
         }
+        
+        [Theory]
+        [InlineData(EventTypes.ContractCreatedEvent)]
+        [InlineData(EventTypes.ContractUpdatedEvent)]
+        public void ContractNotAddedToAssetWhenNoUnapprovedContractsAreAvailable(string eventType)
+        {
+            var contractId = Guid.NewGuid();
+            var assetId = Guid.NewGuid();
+            this.Given(g => _ContractsApiFixture.GivenApprovedContractsAreReturned(contractId, assetId))
+                .And(g => _AssetApiFixture.GivenTheAssetExists(assetId))
+                .And(g => _esFixture.GivenAnAssetIsIndexed(assetId.ToString()))
+                .When(w => _steps.WhenTheFunctionIsTriggered(contractId, eventType, assetId.ToString()))
+                .Then(t => _steps.ThenTheAssetInTheIndexIsUpdatedAndHasNoContracts(_AssetApiFixture.ResponseObject, 
+                    _esFixture.ElasticSearchClient))
+                .BDDfy();
+        }
     }
 }
