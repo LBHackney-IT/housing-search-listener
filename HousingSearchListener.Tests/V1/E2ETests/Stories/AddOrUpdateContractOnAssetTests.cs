e6ff2388a03d0bb337207dacc5729f954e6a761d
James Heppinstall <james.heppinstall@hackney.gov.uk>
2025-01-31 11:02:03 +0000

    all tests back in

--- HousingSearchListener.Tests/V1/E2ETests/Stories/AddOrUpdateContractOnAssetTests.cs
+++ HousingSearchListener.Tests/V1/E2ETests/Stories/AddOrUpdateContractOnAssetTests.cs
@@ -76,36 +76,36 @@ namespace HousingSearchListener.Tests.V1.E2ETests.Stories
                 .BDDfy();
         }
 
-        // [Theory]
-        // [InlineData(EventTypes.ContractCreatedEvent)]
-        // [InlineData(EventTypes.ContractUpdatedEvent)]
-        // public void ContractAddedToAsset(string eventType)
-        // {
-        //     var contractId = Guid.NewGuid();
-        //     var assetId = Guid.NewGuid();
-        //     // this.Given(g => _ContractsApiFixture.GivenMultipleContractsAreReturned(contractId, assetId))
-        //     this.Given(g => _AssetApiFixture.GivenTheAssetExists(assetId))
-        //         .And(g => _esFixture.GivenAnAssetIsIndexed(assetId.ToString()))
-        //         .When(w => _steps.WhenTheFunctionIsTriggered(contractId, eventType, assetId.ToString()))
-        //         .Then(t => _steps.ThenTheAssetInTheIndexIsUpdatedWithTheContracts(_AssetApiFixture.ResponseObject,
-        //             _ContractsApiFixture.ResponseObject, _esFixture.ElasticSearchClient))
-        //         .BDDfy();
-        // }
+        [Theory]
+        [InlineData(EventTypes.ContractCreatedEvent)]
+        [InlineData(EventTypes.ContractUpdatedEvent)]
+        public void ContractAddedToAsset(string eventType)
+        {
+            var contractId = Guid.NewGuid();
+            var assetId = Guid.NewGuid();
+            // this.Given(g => _ContractsApiFixture.GivenMultipleContractsAreReturned(contractId, assetId))
+            this.Given(g => _AssetApiFixture.GivenTheAssetExists(assetId))
+                .And(g => _esFixture.GivenAnAssetIsIndexed(assetId.ToString()))
+                .When(w => _steps.WhenTheFunctionIsTriggered(contractId, eventType, assetId.ToString()))
+                .Then(t => _steps.ThenTheAssetInTheIndexIsUpdatedWithTheContracts(_AssetApiFixture.ResponseObject,
+                    _ContractsApiFixture.ResponseObject, _esFixture.ElasticSearchClient))
+                .BDDfy();
+        }
 
-        // [Theory]
-        // [InlineData(EventTypes.ContractCreatedEvent)]
-        // [InlineData(EventTypes.ContractUpdatedEvent)]
-        // public void ContractNotAddedToAssetWhenNoUnapprovedContractsArePresent(string eventType)
-        // {
-        //     var contractId = Guid.NewGuid();
-        //     var assetId = Guid.NewGuid();
-        //     this.Given(g => _ContractsApiFixture.GivenApprovedContractsAreReturned(contractId, assetId))
-        //         .And(g => _AssetApiFixture.GivenTheAssetExists(assetId))
-        //         .And(g => _esFixture.GivenAnAssetIsIndexed(assetId.ToString()))
-        //         .When(w => _steps.WhenTheFunctionIsTriggered(contractId, eventType, assetId.ToString()))
-        //         .Then(t => _steps.ThenTheAssetInTheIndexIsUpdatedAndHasNoContracts(_AssetApiFixture.ResponseObject,
-        //             _esFixture.ElasticSearchClient))
-        //         .BDDfy();
-        // }
+        [Theory]
+        [InlineData(EventTypes.ContractCreatedEvent)]
+        [InlineData(EventTypes.ContractUpdatedEvent)]
+        public void ContractNotAddedToAssetWhenNoUnapprovedContractsArePresent(string eventType)
+        {
+            var contractId = Guid.NewGuid();
+            var assetId = Guid.NewGuid();
+            this.Given(g => _ContractsApiFixture.GivenApprovedContractsAreReturned(contractId, assetId))
+                .And(g => _AssetApiFixture.GivenTheAssetExists(assetId))
+                .And(g => _esFixture.GivenAnAssetIsIndexed(assetId.ToString()))
+                .When(w => _steps.WhenTheFunctionIsTriggered(contractId, eventType, assetId.ToString()))
+                .Then(t => _steps.ThenTheAssetInTheIndexIsUpdatedAndHasNoContracts(_AssetApiFixture.ResponseObject,
+                    _esFixture.ElasticSearchClient))
+                .BDDfy();
+        }
     }
 }
